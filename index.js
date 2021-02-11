f8f26382e3f6d8a17b1315d9e99aa7e636f25f55
Anh Minh <anthonyle9x@gmail.com>
2021-02-11 22:12:50 +0700

    Updated port

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get("/", function (req, res) {
   res.send("Hello, why are you here?");
 });
 
-app.listen(4000, () => {
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
   console.log("Listening on port 4000.");
 });
